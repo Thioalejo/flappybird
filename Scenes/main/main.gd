66b6e7f52f7e8dfd403ce42f127eb4b9902d6a51
Thioalejo <alejo.saraza@gmail.com>
2025-03-30 18:04:19 -0500

    Se crea ground del juego, movimiento

--- Scenes/main/main.gd
+++ Scenes/main/main.gd
@@ -7,10 +7,8 @@ extends Node2D
 func _on_player_on_game_started() -> void:
 	spawner.timer.start()
 
-
-func _on_obstacle_on_player_crashed() -> void:
-	print("Colision")
-
-
 func _on_spawner_on_obstacle_crashed() -> void:
 	player.stop_movement()
+
+func _on_ground_on_player_crsh() -> void:
+	spawner.stop_obstacles()
